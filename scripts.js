55498b9567fb6bc53ca68f96787b2a3dec2cd969
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-06-22 00:10:05 +0200

    some more improvements

--- scripts.js
+++ scripts.js
@@ -1,17 +1,24 @@
 console.log('1');
 
+
+// Selectors
 const dropdown = document.querySelector('.menu');
 const hidden = document.querySelector('.hidden');
 const close = document.querySelector('.close');
+const products = document.querySelector('.products')
 
+// Event Listeners
 dropdown.addEventListener('click', popUp);
+close.addEventListener('click', closeFunction);
+products.addEventListener('click', popUp);
 
+//Functions
 function popUp() {
     //hidden.style.display = "block";
     hidden.style.transform = "scale(1)";
 };
 
-close.addEventListener('click', closeFunction);
+
 
 function closeFunction() {
     hidden.style.transform = "scale(0)";
